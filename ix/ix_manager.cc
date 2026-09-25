f698e9c431578623248ddae30204d40d7689aa3e
brian-io <brianmikwaa@gmail.com>
2026-09-25 22:02:00 +0300

    indexing manager implentation

--- ix/ix_manager.cc
+++ ix/ix_manager.cc
@@ -6,18 +6,45 @@
 #include "ix_internal.h"
 
 
-class IX_Manager {
-  public:
-       IX_Manager   (PF_Manager &pfm);              // Constructor
-       ~IX_Manager  ();                             // Destructor
-    RC CreateIndex  (const char *fileName,          // Create new index
-                     int        indexNo,
-                     AttrType   attrType,
-                     int        attrLength);
-    RC DestroyIndex (const char *fileName,          // Destroy index
-                     int        indexNo);
-    RC OpenIndex    (const char *fileName,          // Open index
-                     int        indexNo,
-                     IX_IndexHandle &indexHandle);
-    RC CloseIndex   (IX_IndexHandle &indexHandle);  // Close index
-};
+// Constructor
+IX_Manager::IX_Manager(PF_Manager &pfm)
+{
+
+}
+
+// Destructor
+IX_Manager::~IX_Manager()
+{
+  
+}
+
+RC IX_Manager::CreateIndex(
+  const char *fileName,          // Create new index
+  int        indexNo,
+  AttrType   attrType,
+  int        attrLength){
+    
+
+    if (fileName == nullptr)
+      return IX_INVALIDFILE;
+  
+}
+
+RC IX_Manager::DestroyIndex(
+  const char *fileName,
+  int        indexNo
+){
+  
+}
+
+RC IX_Manager::OpenIndex(
+  const char *fileName,         
+  int        indexNo,
+  IX_IndexHandle &indexHandle
+){
+  
+}
+
+RC IX_Manager::CloseIndex(IX_IndexHandle &fileName){
+  
+}
